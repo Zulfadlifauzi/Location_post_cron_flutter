b49033094dc8532e73f93d0708a5dcfe5384fc08
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-09-17 06:52:29 +0800

    add lottie and dependencies

--- userlocation/lib/screens/home.dart
+++ userlocation/lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -25,13 +26,26 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Column(
         children: <Widget>[
           Padding(
-            padding: const EdgeInsets.only(left: 10.0, top: 20.0),
-            child: Text(
+            padding: const EdgeInsets.only(top: 20.0),
+            child: Container(
+              child: Text(
                 'Enable your location ! \nFor better live shipping experience',
                 style: GoogleFonts.secularOne(
                   fontSize: 20,
-                )),
+                ),
+              ),
+            ),
           ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30.0, top: 30),
+            child: Container(
+              child: Lottie.asset(
+                'assets/lottie/delivery.json',
+                width: 350,
+                height: 200,
+              ),
+            ),
+          )
         ],
       ),
     );
